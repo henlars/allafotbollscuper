b73236b2afa68f59a0efd431f958a06e60a793a8
henlars <henriklarssonmail@gmail.com>
2024-10-14 08:56:54 +0200

    adjusted link behaviour when clicking on a blank link

--- src/app/components/Link.js
+++ src/app/components/Link.js
@@ -1,5 +1,6 @@
 'use client';
 import { Link } from '@chakra-ui/next-js';
+import { Box } from '@chakra-ui/react';
 
 export default function ChakraNextLink({ href, newTab, children }) {
   return (
@@ -8,6 +9,7 @@ export default function ChakraNextLink({ href, newTab, children }) {
       _hover={href ? { cursor: 'pointer' } : { cursor: 'not-allowed' }}
       target={newTab && '_blank'}
       width={'100%'}
+      style={{ pointerEvents: !href && 'none' }}
     >
       {children}
     </Link>
